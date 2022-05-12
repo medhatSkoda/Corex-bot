6ae7a62cfa18db986e89b140f02375fa7fc0b40d
LukaH0 <luka.hietala08@gmail.com>
2022-05-12 16:53:53 +0300

    Setup for Discord js and dotenv.

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 //NPM imports
+const { Client, Intents } = require('discord.js');
 const dotenv = require('dotenv');
 
 //Setup
 dotenv.config();
-
-const { Client, Intents } = require('discord.js');
 
 //Create client and intents
 
 const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS, 
-        Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_BANS,
-        Intents.FLAGS.GUILD_EMOJIS,
-        Intents.FLAGS.GUILD_INVITES,
-        Intents.FLAGS.GUILD_PRESENCES,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        Intents.FLAGS.DIRECT_MESSAGES,
-        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
     ] 
 });
+
+//Important variables
+const token = process.env.TOKEN;
+
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord
+client.login(token);
