9afa0f5ec6281bcba1ae7ef1501b3ea6e76d98cf
LukaH0 <luka.hietala08@gmail.com>
2022-05-12 16:44:50 +0300

    Added main Intents

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const dotenv = require('dotenv');
 
 //Setup
 dotenv.config();
+
+const { Client, Intents } = require('discord.js');
+
+//Create client and intents
+
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS, 
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_BANS,
+        Intents.FLAGS.GUILD_EMOJIS,
+        Intents.FLAGS.GUILD_INVITES,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    ] 
+});
